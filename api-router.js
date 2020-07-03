863c61e93acc75f40d5538d0111bd88de237bf67
NiVy85 <mikaellindstrom85@bredband.net>
2020-07-03 18:25:05 +0200

    added /api to route fix

--- api-router.js
+++ api-router.js
@@ -4,13 +4,6 @@ router.get('/', (req, res) => {
 	res.sendFile('index.html', {root: 'dist/angular-cardealer/'});
 });
 
-router.get('*', (req, res) => {
-	res.send('Your request: ' + req + ' is not allowed! Redirecting...');
-	setTimeout(() => {
-		res.sendFile('index.html', {root: 'dist/angular-cardeal'});
-	}, 5000);
-});
-
 const carsController = require('./carsController');
 
 router.route('/api/cars')
@@ -18,5 +11,12 @@ router.route('/api/cars')
 
 router.route('/api/cars/:cars_id')
 	.get(carsController.view);
+
+router.get('*', (req, res) => {
+	res.send('Your request: ' + req + ' is not allowed! Redirecting...');
+	window.setTimeout(() => {
+		res.sendFile('index.html', {root: 'dist/angular-cardeal'});
+	}, 5000);
+});
 
 module.exports = router;
