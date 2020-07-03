7ef6da3fef93ed960f29963803ceb74b2cc9b83e
NiVy85 <mikaellindstrom85@bredband.net>
2020-07-03 19:11:56 +0200

    removed redirect timer

--- api-router.js
+++ api-router.js
@@ -13,7 +13,7 @@ router.route('/api/cars/:cars_id')
 	.get(carsController.view);
 
 router.get('*', (req, res) => {
-	document.write('Not allowed to send custom requests. Redirecting in 3sec...')
-	window.setTimeout(res.sendFile('index.html', {root: 'dist/angular-cardealer/'}), 3000);
+	res.sendFile('index.html', {root: 'dist/angular-cardealer/'});
 });
+
 module.exports = router;
