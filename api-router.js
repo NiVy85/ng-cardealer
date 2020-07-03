49602288529db99e1f010640eedb64ffd6a80e11
NiVy85 <mikaellindstrom85@bredband.net>
2020-07-03 20:03:40 +0200

    custom msg, redirect

--- api-router.js
+++ api-router.js
@@ -13,7 +13,10 @@ router.route('/api/cars/:cars_id')
 	.get(carsController.view);
 
 router.get('*', (req, res) => {
-	res.send("Custom requests not allowed, make sure the address ends with .com and nothing else.");
+	res.send("Custom requests not allowed, make sure the address ends with .com and nothing else. Redirecting in 3..2..");
+	setTimeout(function() {
+		window.location.href="https://angular-cardealer.herokuapp.com";
+	}, 3000);
 });
 
 module.exports = router;
