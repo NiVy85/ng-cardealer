fc333953c276dedcef9841f7075075281fdb1a19
NiVy85 <mikaellindstrom85@bredband.net>
2020-07-03 19:05:43 +0200

    redirect

--- api-router.js
+++ api-router.js
@@ -13,10 +13,7 @@ router.route('/api/cars/:cars_id')
 	.get(carsController.view);
 
 router.get('*', (req, res) => {
-	res.send('Your request: ' + req + ' is not allowed! Redirecting...');
-	window.setTimeout(() => {
-		res.sendFile('index.html', {root: 'dist/angular-cardeal'});
-	}, 5000);
+	document.write('Not allowed to send custom requests. Redirecting in 3sec...')
+	window.setTimeout(res.sendFile('index.html', {root: 'dist/angular-cardealer/'}), 3000);
 });
-
 module.exports = router;
