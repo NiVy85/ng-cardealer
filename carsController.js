52565b3dd009c5a00864a0c7210e30152653e874
NiVy85 <mikaellindstrom85@bredband.net>
2020-07-06 20:20:50 +0200

    fix wierd error

--- carsController.js
+++ carsController.js
@@ -44,18 +44,20 @@ exports.addcar = (req, res, next) => {
 exports.delcar = (req, res) => {
 	let car = { regnr: req.body.regnrdel};
 	staticCars = ["ARG678", "TTY199", "KLI082", "UUP123", "WLK131", "SLP764"];
-	staticCars.forEach( element => {
-		if(car == element)
+	for (const element of staticCars) {
+		if(element == car)
 			return {error: "error", message: "Not allowed to remove that car"};
-	});
-	Cars.find(car).remove((err) => {
-		if(err)
-			res.send(err)
-		else
-			res.json({
-				status: "success",
-				message: "Car has been removed successfully",
-				data: car
+		else {
+			Cars.find(car).remove((err) => {
+				if(err)
+					res.send(err)
+				else
+					res.json({
+						status: "success",
+						message: "Car has been removed successfully",
+						data: car
+				});
 			});
-	});
+		}
+	};
 }
